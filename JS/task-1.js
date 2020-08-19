d9ac4d4279a9effe406795b59a30af3fd105f03b
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-08-19 21:31:55 +0300

    добавляет работу номер 2

---
+++ JS/task-1.js
@@ -0,0 +1,10 @@
+"use strict";
+const getItemsString = function (array) {
+  "use strict";
+  for (const item of getItemsString) {
+    let n = 0;
+    let result = `${n + 1} - ${item}\n `;
+  }
+  return result;
+};
+console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
