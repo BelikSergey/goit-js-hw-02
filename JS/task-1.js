8ec6131f38e302b46462047a851f2ef78ded95ae
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-08-21 23:46:14 +0300

    добавляет решение к задаче 1 2 3 . частично 4

--- JS/task-1.js
+++ JS/task-1.js
@@ -1,10 +1,11 @@
 "use strict";
+let result = "";
 const getItemsString = function (array) {
-  "use strict";
-  for (const item of getItemsString) {
-    let n = 0;
-    let result = `${n + 1} - ${item}\n `;
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${i + 1} - ${array[i]}\n`;
   }
   return result;
 };
+
 console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
+console.log(getItemsString([5, 10, 15]));
--- JS/task-4.js
+++ JS/task-4.js
@@ -1,1 +1,20 @@
 "use strict";
+function formatString(string, maxLength = 40) {
+  let arrey = string.split("");
+  if (arrey.lenght < maxLength) {
+    return arrey.join("");
+  } else {
+    arrey.lenght = maxLength;
+    return arrey.join("") + `...`;
+  }
+}
+
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// 'Curabitur ligula sapien, tincidunt non.'
+
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// 'Vestibulum facilisis, purus nec pulvinar...'
+
+// console.log(
+//   formatString("Vestibulum facilisis, purus nec pulvinar iaculis.", 30));
+// 'Vestibulum facilisis, purus ne...'
