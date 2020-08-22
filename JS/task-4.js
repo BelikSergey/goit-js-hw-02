46c96c32333e95a559300982759e65a6312f03f6
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-08-22 22:35:23 +0300

    добавляет решение по остальным задачам

--- JS/task-4.js
+++ JS/task-4.js
@@ -1,20 +1,21 @@
 "use strict";
 function formatString(string, maxLength = 40) {
-  let arrey = string.split("");
-  if (arrey.lenght < maxLength) {
-    return arrey.join("");
+  const arrey = string.split("");
+  if (arrey.length > maxLength) {
+    return arrey.slice(0, maxLength).join("") + "...";
   } else {
-    arrey.lenght = maxLength;
-    return arrey.join("") + `...`;
+    return arrey.join("");
   }
 }
 
+// console.log(formatString);
 console.log(formatString("Curabitur ligula sapien, tincidunt non."));
 // 'Curabitur ligula sapien, tincidunt non.'
 
 console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
 // 'Vestibulum facilisis, purus nec pulvinar...'
 
-// console.log(
-//   formatString("Vestibulum facilisis, purus nec pulvinar iaculis.", 30));
+console.log(
+  formatString("Vestibulum facilisis, purus nec pulvinar iaculis.", 30)
+);
 // 'Vestibulum facilisis, purus ne...'
